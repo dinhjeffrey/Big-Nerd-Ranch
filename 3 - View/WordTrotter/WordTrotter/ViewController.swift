ec2705510634d8dfe43acfe6b5997ec982d1945a
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-28 10:44:38 -0700

    added view programmatically

--- 3 - View/WordTrotter/WordTrotter/ViewController.swift
+++ 3 - View/WordTrotter/WordTrotter/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // To create a CGRect, you use its initializer and pass in values for x,y,width,height
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        // creating the view using the dimension of firstFrame
+        let firstView = UIView(frame: firstFrame)
+        // setting the background color on the view
+        firstView.backgroundColor = UIColor.blueColor()
+        // adding subview to view
+        view.addSubview(firstView)
+    }
 
 
 }
