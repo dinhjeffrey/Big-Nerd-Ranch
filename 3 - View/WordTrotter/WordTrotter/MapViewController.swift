498fda8eb3a91000f33ee35da1e6835f78b693cd
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-01 10:45:36 -0700

    added more annotation pins and created button programmatically

--- 3 - View/WordTrotter/WordTrotter/MapViewController.swift
+++ 3 - View/WordTrotter/WordTrotter/MapViewController.swift
@@ -12,7 +12,8 @@ import MapKit
 class MapViewController: UIViewController, MKMapViewDelegate {
     
     // Constants and variables
-    let mapView = MKMapView()
+    let mapView = MKMapView() // creates mapView programmatically
+    var isHighLighted = false // to highlight and unhighlight button
 
     // View Controller lifecycle
     override func viewDidLoad() {
@@ -23,9 +24,6 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     // when view controller is created, its view property is nil
     // when a view controller is asked for its view and its view is nil, loadView() method is called
     override func loadView() {
-        // create a map view
-        
-        
         // set it as the view of this view controller
         view = mapView
         
@@ -47,14 +45,15 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         // creates a constraint between two anchors
         // top layout guide will allow content to not underlap the status bar or navigation bar at the top of the screen
         let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
-        let margins = view.layoutMarginsGuide
+        var margins = view.layoutMarginsGuide
         let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
-        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+        var trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
         
         topConstraint.active = true
         leadingConstraint.active = true
         trailingConstraint.active = true
         
+        // Setting map view
         let latitude:CLLocationDegrees = 33.6086549608 //insert latitutde
         
         let longitude:CLLocationDegrees = -117.929614227 //insert longitude
@@ -71,11 +70,45 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         
         mapView.setRegion(region, animated: true)
         
+        // Creating Pins
         let dropPin = MKPointAnnotation()
         dropPin.coordinate = location
         dropPin.title = "Newport Beach"
         dropPin.subtitle = "Pokemon Go ^^"
         mapView.addAnnotation(dropPin)
+        
+        let dropPin2 = MKPointAnnotation()
+        dropPin2.coordinate = CLLocationCoordinate2D(latitude: 33.607220, longitude: -117.928491)
+        dropPin2.title = "Sand"
+        dropPin2.subtitle = "Dratini"
+        mapView.addAnnotation(dropPin2)
+        
+        let dropPin3 = MKPointAnnotation()
+        dropPin3.coordinate = CLLocationCoordinate2D(latitude: 33.609945, longitude: -117.928416)
+        dropPin3.title = "Restaurants"
+        dropPin3.subtitle = "PokeStops"
+        mapView.addAnnotation(dropPin3)
+        
+        // create a button programmatically
+        let btn: UIButton = UIButton(frame: CGRectMake(100, 400, 100, 50))
+        btn.layer.cornerRadius = 0.05 * btn.bounds.size.width
+        btn.backgroundColor = UIColor.orangeColor()
+        btn.setTitle("Cycle", forState: UIControlState.Normal)
+        btn.setTitleColor(UIColor.blackColor(), forState: .Highlighted)
+        btn.addTarget(self, action: #selector(MapViewController.cycleAnnotations(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        btn.translatesAutoresizingMaskIntoConstraints = false // set autolayout size dynamically
+        view.addSubview(btn) // add to view as subview
+        
+        // create button constraints
+        let bottomConstraint = btn.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.bottomAnchor, constant: -50)
+        margins = view.layoutMarginsGuide
+        //leadingConstraint = btn.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+        trailingConstraint = btn.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+        
+        bottomConstraint.active = true
+        //leadingConstraint.active = true
+        trailingConstraint.active = true
+
     }
     
     // Methods
@@ -90,6 +123,17 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         default:
             break
         }
+    }
+    
+    func cycleAnnotations(sender: UIButton) {
+        if isHighLighted == false {
+            sender.highlighted = true;
+            isHighLighted = true
+        } else {
+            sender.highlighted = false;
+            isHighLighted = false
+        }
+        print("cycling")
     }
     
     
